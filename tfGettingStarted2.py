d15cc3eae3f3084a52373643528e7acb841be23a
SilasPillsbury <30637327+SilasPillsbury@users.noreply.github.com>
2018-01-22 12:13:08 -0500

    Made Things Actually work

--- tfGettingStarted2.py
+++ tfGettingStarted2.py
@@ -1,25 +1,33 @@
 import tensorflow as tf
 
 #initialize variables
-sess = tf.Session()
+def run(inde=[1,2,3,4],depe=[2,4,6,8]):
+  sess = tf.Session()
 
-W = tf.Variable([0.3], dtype=tf.float32)
-b = tf.Variable([-0.3], dtype=tf.float32)
-x = tf.placeholder(tf.float32)
-y = tf.placeholder(tf.float32)
+  M = tf.Variable([0.4], dtype=tf.float32)
+  W = tf.Variable([0.3], dtype=tf.float32)
+  b = tf.Variable([-0.3], dtype=tf.float32)
+  x = tf.placeholder(tf.float32)
+  y = tf.placeholder(tf.float32)
 
-#functions and operations
-linear_model = W*x + b
-squared_deltas = tf.square(linear_model - y)
-loss = tf.reduce_sum(squared_deltas)
 
-#train/run
-optimizer = tf.train.GradientDescentOptimizer(0.01)
-train = optimizer.minimize(loss)
-init = tf.global_variables_initializer()
-sess.run(init)
-for i in range(1000):
-  sess.run(train, {x: [1,2,3,4], y: [0,-1,-2,-3]})
+  #functions and operations
+  
+  linear_model = W*x + b + M*x*x*x*x*x*10**-100
+  #linear_model = M*tf.square(x) + W*x + b + 5
+  squared_deltas = tf.square(linear_model - y)
+  loss = tf.reduce_sum(squared_deltas)
 
-print(sess.run([W,b]))
-print(sess.run(loss, {x: [1,2,3,4], y: [0,-1,-2,-3]}))
+  #train/run
+  optimizer = tf.train.GradientDescentOptimizer(0.01)
+  train = optimizer.minimize(loss)
+  init = tf.global_variables_initializer()
+  sess.run(init)
+  for i in range(1000):
+    sess.run(train, {x: inde, y: depe})
+
+  print(sess.run([M,W,b]))
+  print(sess.run(loss, {x: inde, y: depe}))
+  return M, W, b, x, y
+
+run()
