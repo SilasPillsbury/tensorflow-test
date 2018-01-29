e24d6b7ea31d979332b7539207014fd7434e49ac
SilasPillsbury <30637327+SilasPillsbury@users.noreply.github.com>
2018-01-29 10:36:47 -0500

    Added Cubic, Changed Step Size
    Also changed input and ouput

--- tfGettingStarted2.py
+++ tfGettingStarted2.py
@@ -1,9 +1,10 @@
 import tensorflow as tf
 
 #initialize variables
-def run(inde=[1,2,3,4],depe=[2,4,6,8]):
+def run(inde=[1,2,3,4],depe=[2,5,10,17]):
   sess = tf.Session()
 
+  Q = M = tf.Variable([0.4], dtype=tf.float32)
   M = tf.Variable([0.4], dtype=tf.float32)
   W = tf.Variable([0.3], dtype=tf.float32)
   b = tf.Variable([-0.3], dtype=tf.float32)
@@ -13,20 +14,20 @@ def run(inde=[1,2,3,4],depe=[2,4,6,8]):
 
   #functions and operations
   
-  linear_model = W*x + b + M*x*x*10**-1
+  linear_model = Q*x*x*x + M*x*x + W*x + b
   #linear_model = M*tf.square(x) + W*x + b + 5
   squared_deltas = tf.square(linear_model - y)
   loss = tf.reduce_sum(squared_deltas)
 
   #train/run
-  optimizer = tf.train.GradientDescentOptimizer(0.01)
+  optimizer = tf.train.GradientDescentOptimizer(0.0001)
   train = optimizer.minimize(loss)
   init = tf.global_variables_initializer()
   sess.run(init)
-  for i in range(1000):
+  for i in range(10000):
     sess.run(train, {x: inde, y: depe})
 
-  print(sess.run([M,W,b]))
+  print("Q,M,W,b:",sess.run([Q,M,W,b]))
   print(sess.run(loss, {x: inde, y: depe}))
   return M, W, b, x, y
 
